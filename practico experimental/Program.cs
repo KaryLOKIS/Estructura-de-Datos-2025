0d2ff5199faa509e631c39f37c4b8f939dd9d394
karina <ke.rumipambag@uea.edu.ec>
2025-06-21 23:42:56 -0500

    creamos un pequeño menu con opcines a elegir

--- practico experimental/Program.cs
+++ practico experimental/Program.cs
@@ -2,4 +2,23 @@
 Console.WriteLine("Universidad Estatal Amazonica");
 Console.WriteLine("Practico Experimental 1");
 
+System.Console.WriteLine("Agenda de turnos de Pacientes");
 
+int max = 10;
+Paciente[] agenda =  Paciente[max];
+int contador = 0;
+
+bool salir = false;
+
+while (!salir)
+{
+    System.Console.WriteLine("\nMenu de opciones");
+    System.Console.WriteLine("1. Registrar turno");
+    System.Console.WriteLine("2. Ver turnos registrados");
+    System.Console.WriteLine("3. buscar paciente por nombre");
+    System.Console.WriteLine("4. salir");
+    System.Console.WriteLine("seleccione una opcion");
+
+    string opcion = System.Console.WriteLine();
+
+}
