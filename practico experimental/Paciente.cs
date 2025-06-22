2e31c516f24fd1136322da6da3655965ef3552b2
karina <ke.rumipambag@uea.edu.ec>
2025-06-22 01:15:55 -0500

    Ultimo arreglo

--- practico experimental/Paciente.cs
+++ practico experimental/Paciente.cs
@@ -1,17 +1,16 @@
 
 public class Paciente
 {
-    public string Nombre { get; set }
-    public string Cedula { get; set }
-    public int Edad { get; set }
-    public string MotivoConsulta { get; set }
+    public string Nombre { get; set; }
+    public string Cedula { get; set; }
+    public int Edad { get; set; }
+    public string MotivoConsulta { get; set; }
 
-    public Paciente(string nombre, string cedula, int edad, string motivo)
+    public Paciente (string nombre, string cedula, int edad, string motivo)
     {
         Nombre = nombre;
         Cedula = cedula;
         Edad = edad;
         MotivoConsulta = motivo;
     }
-
 }
